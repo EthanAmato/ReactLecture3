b3e15cba0c93a7232eacf429c6898ceb0831629d
EthanAmato <ethannamato@gmail.com>
2023-03-21 14:32:13 -0400

    added vercel.json for proper deployment of paths

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
     {/* BrowserRouter: BrowserRouter is a router implementation that uses the HTML5 history API 
     (pushstate, replacestate, and popstate events) to keep your UI in sync with the URL.
      It is the parent component used to store all other components. Route: This is a new 
